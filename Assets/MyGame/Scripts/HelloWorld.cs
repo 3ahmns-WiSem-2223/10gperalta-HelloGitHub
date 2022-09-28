28eb6e1d3b0a614d7ea1a93da76833d8e7bd83ad
GlendaLP <114597911+GlendaLP@users.noreply.github.com>
2022-09-28 17:37:13 +0200

    Add files via upload

---
+++ Assets/MyGame/Scripts/HelloWorld.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+}
---
+++ Assets/MyGame/Scripts/HelloWorldneu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HelloWorldneu : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+}
